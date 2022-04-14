a4daf5226bebf6badd8c3830e5c33acf4a191c9c
Danial Erfanian <erfaniiyanerfan@gmail.com>
2022-04-14 11:59:39 +0430

    Add simple parenthese recognizer

--- compiler.py
+++ compiler.py
@@ -1,2 +1,16 @@
 # MN
 # DE
+from scanner import Scanner
+
+
+def run():
+    scanner = Scanner()
+    with open("PA1-Testcases/T01/input.txt", "r") as f:
+        while next_char := f.read(1):
+            recognized_token = scanner.get_next_token(next_char)
+            if recognized_token:
+                print(recognized_token)
+
+
+if __name__ == '__main__':
+    run()
---
+++ scanner.py
@@ -0,0 +1,41 @@
+from collections import namedtuple
+from enum import Enum
+
+StateItem = namedtuple('State', ['id', 'accept', 'lookahead'])
+
+
+class TokenType(Enum):
+    SYMBOL = 0
+
+
+class State(Enum):
+    INITIAL = StateItem(0, False, False)
+    ACC = StateItem(1, True, False)
+
+
+class DFA:
+    initial_state = State.INITIAL
+    next = {
+        (State.INITIAL, '('): State.ACC,
+        (State.INITIAL, ')'): State.ACC,
+        # (state, character): state2,
+    }
+
+    @staticmethod
+    def get_next_state(current, next_char):
+        return DFA.next.get((current, next_char)) or State.INITIAL
+
+
+class Scanner:
+    def __init__(self):
+        self.current = DFA.initial_state  # INITIAL
+        self.buffer = ""
+
+    def get_next_token(self, next_char):
+        self.current = DFA.get_next_state(self.current, next_char)
+        self.buffer = self.buffer + next_char
+        if self.current.value.accept:
+            result = (TokenType.SYMBOL, self.buffer)
+            self.buffer = ""
+            self.current = DFA.initial_state
+            return result
