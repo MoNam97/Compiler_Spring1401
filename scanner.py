0fcca1b7f3ad747a73f41ceb8c3a094465ace152
Danial Erfanian <erfaniiyanerfan@gmail.com>
2022-04-14 12:43:24 +0430

    set chars

--- scanner.py
+++ scanner.py
@@ -1,16 +1,33 @@
 from collections import namedtuple
 from enum import Enum
+from string import ascii_letters, digits, whitespace
 
-StateItem = namedtuple('State', ['id', 'accept', 'lookahead'])
 
+# TODO:
+# [ ] Handle Invalid Inputs
+# [ ] next_state should consider sets
+# [ ] Remove last charracter hack
 
 class TokenType(Enum):
     SYMBOL = 0
 
 
+StateItem = namedtuple('State', ['id', 'accept', 'lookahead'])
+
+
 class State(Enum):
     INITIAL = StateItem(0, False, False)
     ACC = StateItem(1, True, False)
+    EQUAL_SYMBOL = StateItem(2, False, False)
+    EQUAL_SYMBOL2 = StateItem(3, True, False)
+    EQUAL_SYMBOL3 = StateItem(4, True, True)
+
+
+class Char(Enum):
+    LETTER = set(ascii_letters)
+    DIGIT = set(digits)
+    WHITESPACE = set(whitespace)
+    SYMBOL = set('()[]:*+-=;,<')
 
 
 class DFA:
@@ -18,12 +35,25 @@ class DFA:
     next = {
         (State.INITIAL, '('): State.ACC,
         (State.INITIAL, ')'): State.ACC,
+        (State.INITIAL, '['): State.ACC,
+        (State.INITIAL, ']'): State.ACC,
+        (State.INITIAL, ','): State.ACC,
+        (State.INITIAL, ':'): State.ACC,
+        (State.INITIAL, '+'): State.ACC,
+        (State.INITIAL, '-'): State.ACC,
+        (State.INITIAL, '<'): State.ACC,
+        (State.INITIAL, '='): State.EQUAL_SYMBOL,
+        (State.EQUAL_SYMBOL, '='): State.EQUAL_SYMBOL2,
+        (State.EQUAL_SYMBOL, {Char.LETTER, Char.DIGIT, Char.WHITESPACE, Char.SYMBOL}): State.EQUAL_SYMBOL3,
         # (state, character): state2,
     }
 
     @staticmethod
     def get_next_state(current, next_char):
-        return DFA.next.get((current, next_char)) or State.INITIAL
+        for key, end_state in DFA.next.items():
+            (state, char_set) = key
+            if state == current and next_char in char_set:
+                return end_state
 
 
 class Scanner:
