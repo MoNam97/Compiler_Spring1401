04e714ecb652a9c0c10ad0e5916737f0d2a871a7
Danial Erfanian <erfaniiyanerfan@gmail.com>
2022-04-15 13:11:24 +0430

    Fix wrong lineno in lexical_errors(i.e multi line comment)

--- compiler.py
+++ compiler.py
@@ -1,6 +1,6 @@
 # Danial Erfanian - 97110155
 # Mohamad Namdar  - 97106302
-
+import itertools
 from copy import copy
 
 from lexical_errors import BaseLexicalError
@@ -14,7 +14,7 @@ def write_tokens(recognized_tokens):
         for lineno, token in recognized_tokens:
             if token[0] not in [TokenType.WHITESPACE, TokenType.COMMENT]:
                 if last_line < lineno:
-                    if lineno != 0:
+                    if last_line != -1:
                         f.write('\n')
                     f.write(f"{lineno + 1}. ")
                     last_line = lineno
@@ -49,6 +49,7 @@ def run():
     symbols = copy(IDENTIFIERS)
     lexical_errors = []
     with open("input.txt", "r") as f:
+        last_lineno = 0
         lineno = 0
         while True:
             next_char = f.read(1) or Char.EOF
@@ -57,11 +58,13 @@ def run():
                 while lookahead:
                     recognized_token, lookahead = scanner.get_next_token(next_char)
                     if recognized_token:
-                        recognized_tokens.append((lineno, recognized_token))
+                        recognized_tokens.append((last_lineno, recognized_token))
                         if recognized_token[0] == TokenType.ID and recognized_token[1] not in symbols:
                             symbols.append(recognized_token[1])
+                        last_lineno = lineno
             except BaseLexicalError as e:
-                lexical_errors.append((lineno, e))
+                lexical_errors.append((last_lineno, e))
+                last_lineno = lineno
             if next_char == '\n':
                 lineno += 1
             if next_char == Char.EOF:
